5b823eae8f04bd27d6cd736bcebd7fa0a79ea221
ilyaik <ilya-kyp@yandex.ru>
2020-11-04 12:16:37 +0300

    переделано упр 14

--- PayrollPreparation/Program.cs
+++ PayrollPreparation/Program.cs
@@ -12,7 +12,7 @@ namespace PayrollPreparation
         {
             Console.WriteLine("просто для примера");
 
-            int upr = 14;
+            int[] upr = { 16, 17, 18};
         }
     }
 }

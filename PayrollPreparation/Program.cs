1b1aa1db7dafb0f37919e710e2cc94183da47918
ilyaik <ilya-kyp@yandex.ru>
2020-11-04 11:52:42 +0300

    Добавьте файлы проекта.

---
+++ PayrollPreparation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayrollPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("просто для примера");
+        }
+    }
+}
